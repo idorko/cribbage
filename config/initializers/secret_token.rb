46e310cba885bffaa5678bdbee7294b87cc956e9
Ian Dorko <idorko@gmail.com>
2011-11-17 17:35:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cribbage::Application.config.secret_token = '255280c88a726f2f5491cd580ab5742b6fc21b4627a9f33b2670afd55ddb471c338fb106eb20f7f828719e54d94c162e90a86924305d9ea8d0e3bdcbeac87ed2'
