843a2a82d732c4253f7ef880b0e166a04e17caab
Ian Dorko <idorko@gmail.com>
2011-11-21 16:01:49 -0700

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,13 @@ class User < ActiveRecord::Base
 	def self.authenticate(email, submitted_password)
 	  user = find_by_email(email)
 	  return nil if user.nil?
-	  return user if user.haspassword?(submitted_password)
+	  return user if user.has_password?(submitted_password)
+	end
+	
+	def self.authenticate_with_salt(id, cookie_salt)
+		user = find_by_id(id)
+		return nil if user.nil?
+		return user if user.salt == cookie_salt
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Cribbage::Application.routes.draw do
-  resources :users
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
