48d5dec40e43a688f6c8fb11818fc0702090da76
Ian Dorko <idorko@gmail.com>
2011-11-21 17:30:39 -0700

    Finished user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,19 @@ class UsersController < ApplicationController
 	end
   end
 
+  def edit
+	@user = User.find(params[:id])
+	@title = "Edit User"
+  end
+
+  def update
+	@user = User.find(params[:id])
+	if @user.update_attributes(params[:user])
+		flash[:success] = "Profile updated."
+		redirect_to @user
+	else
+		@title = "Edit User"
+		render 'edit'
+	end
+  end
 end
